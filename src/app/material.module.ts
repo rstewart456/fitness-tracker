8a1996b5f7a78916e32d0af19ac9be44fd1dbf30
Ronald <rstewart456@gmail.com>
2021-03-22 00:01:16 -0400

    Complete section 4. Added sidenav, headers & timer

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,14 @@ import { MatInputModule } from '@angular/material/input'
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatNativeDateModule } from '@angular/material/core'
 import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatListModule } from '@angular/material/list'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatCardModule } from '@angular/material/card'
+import { MatSelectModule } from '@angular/material/select'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatDialogModule } from '@angular/material/dialog'
 
 
 @NgModule({
@@ -16,7 +24,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox'
         MatInputModule, 
         MatDatepickerModule, 
         MatNativeDateModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
     ],
     exports: [
         MatFormFieldModule, 
@@ -25,7 +41,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox'
         MatInputModule, 
         MatDatepickerModule, 
         MatNativeDateModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
     ]
 })
 
